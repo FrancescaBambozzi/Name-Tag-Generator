f778edf684dd23c17433d2d38a7c07254a7e9ccc
FrancescaBambozzi <bambozzif@gmail.com>
2023-01-24 19:14:51 +0100

    Updates

---
+++ precache-manifest.a8a8fbbe33f76e3a70429307419a55a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c204889add6e121d9fd",
+    "url": "/Name-Tag-Generator/static/css/main.a61da426.chunk.css"
+  },
+  {
+    "revision": "5c204889add6e121d9fd",
+    "url": "/Name-Tag-Generator/static/js/main.7ed01fd9.chunk.js"
+  },
+  {
+    "revision": "97736b70401678265d20",
+    "url": "/Name-Tag-Generator/static/js/runtime~main.51cdda84.js"
+  },
+  {
+    "revision": "3248047fe2fb2119e923",
+    "url": "/Name-Tag-Generator/static/js/2.ee7f18c8.chunk.js"
+  },
+  {
+    "revision": "5d9a18a4e2e204bedd20766e21b4c4db",
+    "url": "/Name-Tag-Generator/static/media/nordwood-themes-9a58YsGiTPk-unsplash.5d9a18a4.jpg"
+  },
+  {
+    "revision": "3129d2aacd7f3a13ad9031e0b826e53e",
+    "url": "/Name-Tag-Generator/index.html"
+  }
+];
